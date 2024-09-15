78d8fec6a08d26a425ed7fc52c165e932fb0f674
Arunkumar S <iamsarunn03@gmail.com>
2024-09-15 20:04:09 +0530

    Modified files for deployment on render

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.db import create_all_tables
 from app.routes import app as routes_app
-import os
 
 app = FastAPI()
 
@@ -21,7 +20,3 @@ create_all_tables()
 app.include_router(routes_app)
 
 app.mount('/', StaticFiles(directory='chatbot-frontend/build', html=True), name='static')
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
